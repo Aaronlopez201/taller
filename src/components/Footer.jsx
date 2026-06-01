07530baabe7c36581ca6132aed69f40b155afa9f
Aaronlopez201 <aaronlopez11772@gmail.com>
2026-06-01 11:56:36 -0500

    Revert "no"
    This reverts commit 79a65abfcb7c9270c428088334ecea4a85b91810.

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer" id="contacto">
-      <p>© 2026 Aaron Lopez. Todos los derechos reservados.</p>
-    </footer>
-  );
-}
-
-export default Footer;
